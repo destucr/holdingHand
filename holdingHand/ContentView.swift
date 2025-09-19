81be3a22baac79dc90b0070cdbf467da4d0094e9
destucr <destucr@gmail.com>
2025-09-19 19:50:18 +0700

    Initial Commit

---
+++ holdingHand/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  holdingHand
+//
+//  Created by Destu Cikal Ramdani on 19/09/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ holdingHand/holdingHandApp.swift
@@ -0,0 +1,17 @@
+//
+//  holdingHandApp.swift
+//  holdingHand
+//
+//  Created by Destu Cikal Ramdani on 19/09/25.
+//
+
+import SwiftUI
+
+@main
+struct holdingHandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
